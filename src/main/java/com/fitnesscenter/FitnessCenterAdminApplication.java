cb34bfb3647250e35a1c3ac897b51864200863f6
IT25101763 <it25101763@my.sliit.lk>
2026-05-23 13:10:22 +0530

    Folder structure modification

---
+++ src/main/java/com/fitnesscenter/FitnessCenterAdminApplication.java
@@ -0,0 +1,12 @@
+package com.fitnesscenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessCenterAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessCenterAdminApplication.class, args);
+    }
+}
